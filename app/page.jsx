7383e2351e00925008a13b815c13fde17385c886
djalilsel <selamnia.a.eldjalil@gmail.com>
2024-07-01 23:32:29 +0100

    hero done

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
 import Header from "@/components/landing page/Header";
+import Hero from "@/components/landing page/Hero";
 
 export default function Home() {
   return (
-    <main className="px-[100px] py-[45px]">
-      <Header />
+    <main className="px-[200px] py-[45px] flex flex-col gap-[150px]">
+      <div className="flex flex-col gap-[90px]">
+        <Header />
+        <Hero />
+      </div>
     </main>
   );
 }
---
+++ components/landing page/Hero.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+import cartoon from "@/assets/CARTOON.png";
+
+const Hero = () => {
+  return (
+    <div className="h-[60vh] flex justify-between items-center text-dark">
+      <div className="flex flex-col gap-[32px]">
+        <h1 className="text-5xl headding-light text-gray">
+          Stop wasting time
+          <h1 className="text-5xl headding-bold text-dark">in meetings</h1>
+        </h1>
+        <p className="text-[18px] leading-6">
+          Boost efficiency, save time & money <br /> with post meeting surveys.
+        </p>
+        <div className="grid grid-cols-2 gap-[10px] body-semi">
+          <Link
+            href={"/"}
+            className="text-white border-2 border-main bg-main rounded-[10px] w-[200px] h-[52px] flex justify-center items-center body4-bold"
+          >
+            Get started
+          </Link>
+          <Link
+            href={"/"}
+            className="text-main border-2 border-main rounded-[10px] w-[200px] h-[52px] flex justify-center items-center body4-bold"
+          >
+            Sign up with Google
+          </Link>
+        </div>
+      </div>
+      <img src={cartoon.src} alt="hero" className="select-none" />
+    </div>
+  );
+};
+
+export default Hero;
